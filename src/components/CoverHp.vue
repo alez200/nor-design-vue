7152d7cd5dc9466f5b6fbe348f6ecf9a74589587
Alessandro Balza <alessandro.balza@drivek.com>
2019-05-14 08:51:56 +0200

    created some new components

---
+++ src/components/CoverHp.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+	export default {
+		name: "CoverHp"
+	}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/info.vue
@@ -0,0 +1,29 @@
+<template>
+    <h1 class="[ logo ] [ grid__col grid__col--w50 ]">
+      <a :href="configSiteLogo.href">
+        <img
+            src="../assets/logoBianco.svg"
+            :alt="configSiteLogo.alt"
+            :title="configSiteLogo.title"
+        />
+      </a>
+    </h1>
+</template>
+
+<script>
+
+  export default {
+
+    name: 'Logo',
+
+    props:{
+
+      configSiteLogo:{
+        required: true,
+        type: Object,
+      }
+
+    },
+
+  }
+</script>
